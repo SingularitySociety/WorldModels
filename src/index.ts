7cdae1dde8b859a87baaac3b8163ee69e9848008
isamu <isamu@to-kyo.to>
2024-09-07 09:39:46 +0900

    format

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ const graph_data = {
         " 前人未到のジャングルに、人喰い虎は住んでいるか？",
         "靴紐を引っ張って右足を持ち上げると７０センチ地面から浮き上がりました。靴紐を引っ張って左足を持ち上げると８０センチ地面から浮き上がりました。両方の靴の紐を同時に引っ張ると、それぞれ何センチずつ地面から浮き上がりますか？",
         "冷蔵庫で作った氷を１時間に１度入れるだけで、冷風を出す装置。付属のソラーパネルで中の扇風機を回し、電源は不要。一人暮らしの人にエアコンの代用として販売。価格は通常のエアコンの１０分の１。この装置を評価して。",
-      ]
+      ],
     },
     map: {
       agent: "mapAgent",
@@ -23,25 +23,23 @@ const graph_data = {
           ai: {
             agent: "openAIAgent",
             isResult: true,
-            inputs: {prompt: ":row"}
+            inputs: { prompt: ":row" },
           },
-        }
+        },
       },
-      inputs: {rows: ":inputs"},
+      inputs: { rows: ":inputs" },
       isResult: true,
     },
   },
 };
 
 const main = async () => {
-  
-  const graph = new GraphAI(graph_data, {...llm_agents, ...vanilla_agents});
+  const graph = new GraphAI(graph_data, { ...llm_agents, ...vanilla_agents });
   const result = await graph.run();
-  
-  result.map.ai.map(result => {
+
+  result.map.ai.map((result) => {
     console.log(JSON.stringify(result.choices[0].message.content));
   });
 };
-
 
 main();
